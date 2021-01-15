e4ea4c5aa7ed4d2efe835beca711c3e584efceaf
majespectre00 <oat601_12@hotmail.com>
2021-01-15 16:49:50 +0700

    lab11_1

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int a){
+    if(a == 1){
+        return 1;
+	}else if(a == 0){
+		return 0;
+    }else{
+        return fibonacci(a-1) + fibonacci(a-2);
+    }
 }
